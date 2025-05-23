7839af14e45cdb7e1e61fb055fb366ba6ec535f8
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2025-05-23 23:57:32 +0100

    wip

---
+++ make_torrents.py
@@ -0,0 +1,8 @@
+from torf import Torrent
+
+# Strategy
+# Easy to block by 4096 tiles per bundle on the lower layer.
+# Then we get 16 higher level tiles.
+
+# The higher levels are going to be pretty small?
+# 1 / 256^2 of the
