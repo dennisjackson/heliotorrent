cdaf93f31b65fa1abb19b0da08162b967e4ebcd5
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2025-05-24 17:43:09 +0100

    Refactoring make_torrents

--- make_torrents.py
+++ make_torrents.py
@@ -15,8 +15,6 @@ def get_trackers(url):
         return [x.decode().strip() for x in r.readlines() if len(x) > 1]
 
 def create_torrent(monitoring_path, startIndex, stopIndex, treeSize,trackers=None):
-    if not trackers:
-        trackers = get_trackers(TRACKER_LIST_URL)
     t = Torrent(
         trackers=trackers,
         private=False,
@@ -26,7 +24,7 @@ def create_torrent(monitoring_path, startIndex, stopIndex, treeSize,trackers=Non
 
     paths = [x for x in get_data_tile_paths(startIndex, stopIndex, treeSize)]
     paths += [x for x in get_hash_tile_paths(startIndex, stopIndex, treeSize)]
-    paths = [f"data/{monitoring_path}{file_prefix}/{x}" for x in paths]
+    paths = [f"data/{monitoring_path}/{x}" for x in paths]
     t.filepaths = paths
 
     # Comes last otherwise paths overwrites it.
@@ -36,14 +34,20 @@ def create_torrent(monitoring_path, startIndex, stopIndex, treeSize,trackers=Non
 def get_torrent_path(outdir,monitoring_path,startIndex,stopIndex):
     return f"{outdir}/{monitoring_path}/torrents/{startIndex}-{stopIndex}.torrent"
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 p = url_to_dir("https://tuscolo2026h1.skylight.geomys.org/")
+
 op = get_torrent_path('data',p,0, 4096 * 256)
 if os.path.isfile(op):
-    logging.info(f"{op} already exists}")
+    logging.info(f"{op} already exists")
     exit(0)
 os.makedirs(os.path.dirname(op), exist_ok=True)
 
-t = create_torrent(p, 0, 4096 * 256, 10879387)
+trackers = get_trackers(TRACKER_LIST_URL)
+logging.info(f"Discovered {len(trackers)} trackers from {TRACKER_LIST_URL}")
+
+t = create_torrent(p, 0, 4096 * 256, 10879387,trackers=trackers)
 t.generate()
 t.write(op)
 
