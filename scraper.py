e64480c74314c2c4f8b4ba0d0aa1fd8a6d059ba7
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2025-05-24 17:16:22 +0100

    refactor: remove commented-out documentation from scraper.py

--- scraper.py
+++ scraper.py
@@ -1,20 +1,8 @@
-# Tiles are served as at
-#     <monitoring prefix>/tile/<L>/<N>[.p/<W>]
-# with `Content-Type: application/octet-stream`.
-# `<L>` is the “level” of the tile, and MUST be a decimal ASCII integer between 0
-# and 5, with no additional leading zeroes.
-# `<N>` is the index of the tile within the level. It MUST be a non-negative
-# integer encoded into 3-digit path elements. All but the last path element MUST
-# begin with an `x`. For example, index 1234067 will be encoded as
-# `x001/x234/067`.
-
-
 import argparse
 import logging
 import subprocess
 import sys
 from util import *
-
 
 USER_AGENT = "Heliotorrent v0.0.0 Contact: scraper-reports@dennis-jackson.uk"
 
